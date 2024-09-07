6e8fa6668a671115a1ee139a088b10997403b1f3
Shinkevich Mariya <shinkevich.mr@gmail.com>
2024-09-07 20:50:19 +0300

    правки по кодстайлу

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,10 +2,12 @@
 function getArrayParams(...arr) {
 	if (arr.length === 0) {
 		return 0;
-	}
+	};
 	let min = Infinity;
 	let max = -Infinity;
 	let sum = 0;
+	let avg = 0;
+
 
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] < min) {
@@ -15,7 +17,7 @@ function getArrayParams(...arr) {
 			max = arr[i];
 		}
 		sum += arr[i];
-	}
+	};
 	avg = sum / arr.length;
 
 
@@ -28,78 +30,97 @@ function getArrayParams(...arr) {
 };
 
 console.log(getArrayParams(1, 2, 3, -100, 10));
+
+
 //Задание №2
+
 //Насадка суммирования элементов
 function summElementsWorker(...arr) {
 	let sum = 0;
-	if (arr.length === 0) return 0;
+	if (arr.length === 0) {
+		return 0;
+	};
 	for (let i = 0; i < arr.length; i++) {
 		sum += arr[i];
 	}
 	return sum;
 };
+
 console.log(summElementsWorker(10, 10, 11, 20, 10));
+
 //насадка вычисления разницы максимального и минимального элементов;
 function differenceMaxMinWorker(...arr) {
 	let min = Infinity;
 	let max = -Infinity;
 	let i;
-	if (arr.length === 0) return 0;
+	if (arr.length === 0) {
+		return 0;
+	};
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] < min) {
 			min = arr[i];
-		}
+		};
 		if (arr[i] > max) {
 			max = arr[i];
-		}
+		};
 	}
 
 	return max - min;
 };
 
-console.log(differenceMaxMinWorker(10, 10, 11, 20, 10)); // 20 - 10 => 10
+console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
+
 //насадка вычисления разницы сумм чётных и нечётных элементов;
 function differenceEvenOddWorker(...arr) {
 	let sumEvenElement = 0;
 	let sumOddElement = 0;
 	let i;
-
-	if (arr.length === 0) return 0;
+	if (arr.length === 0) {
+		return 0;
+	};
 
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] % 2 == 0) {
 			sumEvenElement += arr[i];
 		} else {
 			sumOddElement += arr[i];
-		}
-	}
+		};
+	};
 	return sumEvenElement - sumOddElement;
 };
-console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // 114 - 383 => -269
+
+console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
+
 //насадка вычисления среднего значения чётных элементов.
 function averageEvenElementsWorker(...arr) {
 	let sumEvenElement = 0;
 	let countEvenElement = 0;
 	let i;
 
-	if (arr.length === 0) return 0;
+	if (arr.length === 0) {
+		return 0;
+	};
 
 	for (i = 0; i < arr.length; i++) {
 		if ((arr[i] % 2) === 0) {
 			sumEvenElement += arr[i];
 			countEvenElement++;
-		}
-	}
+		};
+	};
 
 	return sumEvenElement / countEvenElement;
 };
+
 console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // [2, 4, 6, 8] => 5
+
 
 //Задание №3
 function makeWork(arrOfArr, func) {
 	let maxWorkerResult = -Infinity;
 
-	if (arrOfArr.length === 0) return 0;
+	if (arr.length === 0) {
+		return 0;
+	};
 
 	for (let i = 0; i < arrOfArr.length; i++) {
 		const maximum = func(...arrOfArr[i]);
